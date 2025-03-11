721c39a8ea95e7142469d2c543e52274d316f211
mk306207 <kolbermateusz@gmail.com>
2025-03-11 13:34:06 +0100

    :o

--- tests.py
+++ tests.py
@@ -23,3 +23,12 @@ async def test_ping_command(bot_client):
         await command(mock_ctx)
 
         mock_ctx.send.assert_called_with("Hi!")
+
+@pytest.mark.asyncio
+async def test_fakePlayer(bot_client):
+    mock_ctx = AsyncMock()
+    mock_ctx.send = AsyncMock()
+    testName = "Cristiano Ronaldo"
+    command = bot_client.get_command('checkPlayer')
+    await command(mock_ctx,name = testName)
+    mock_ctx.send.assert_called_with(f"Player {testName} not found.")
