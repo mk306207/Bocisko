ea76273161e1cfda8d3d175029228415d4c72e0b
mk306207 <kolbermateusz@gmail.com>
2025-03-13 12:20:54 +0100

    more tests

--- tests.py
+++ tests.py
@@ -7,6 +7,7 @@ import pytest
 from unittest.mock import AsyncMock
 from klucze import *
 import sys
+import asyncio
 
 @pytest.fixture
 def bot_client():
@@ -32,3 +33,46 @@ async def test_fakePlayer(bot_client):
     command = bot_client.get_command('checkPlayer')
     await command(mock_ctx,name = testName)
     mock_ctx.send.assert_called_with(f"Player {testName} not found.")
+
+@pytest.mark.asyncio
+async def test_TOP10(bot_client):
+    mock_ctx = AsyncMock()
+    mock_ctx.send = AsyncMock()
+
+    mock_player_data = {
+        "results": [
+            {
+                "player": {"name": "Erling Haaland", "id": 101},
+                "team": {"name": "Manchester City"}
+            },
+            {
+                "player": {"name": "Mohamed Salah", "id": 102},
+                "team": {"name": "Liverpool"}
+            }
+        ]
+    }
+    mock_single_player_data = (10, 5, 0)
+    command = bot_client.get_command("TOP10_PL")
+
+    with patch("main.scraper.PlayerData", return_value=mock_player_data), \
+         patch("main.scraper.SinglePlayer", return_value=mock_single_player_data):
+        
+        await command(mock_ctx)
+
+    mock_ctx.send.assert_any_call("1. Erling Haaland - Manchester City - G/A: 15\n")
+    mock_ctx.send.assert_any_call("2. Mohamed Salah - Liverpool - G/A: 15\n")
+    
+@pytest.mark.asyncio
+async def test_OnJoin(bot_client):
+    mock_member = AsyncMock()
+    mock_member.mention = "<@1234567890>"  # Teraz ustawiamy poprawny format wzmianek
+    mock_member.name = "TestUser"  # Ustawiamy nazwę użytkownika
+    mock_member.id = 1234567890
+    mock_member.__radd__.side_effect = lambda other: other + mock_member.mention
+    mock_channel = AsyncMock()
+    mock_channel.send = AsyncMock()
+    with patch.object(bot_client, "get_channel", return_value=mock_channel):
+        await bot_client.on_member_join(mock_member)
+
+    mock_channel.send.assert_called_once_with(f"Hello <@1234567890>")
+    
